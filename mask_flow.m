97024a12eb217d31fc1bef0975a5bb28a6ace6ae
jo-Huth <jonathan.huth@icloud.com>
2026-03-11 11:39:06 +0100

    delete

--- mask_flow.m
+++
@@ -1,9 +0,0 @@
-function [flow_out] = mask_flow(flow_in, mask)
-
-flow_in_x = flow_in.Vx;
-flow_in_y = flow_in.Vy;
-
-flow_in_x(~mask)= 0;    % Zero invalid regions
-flow_in_y(~mask)= 0;    % Zero invalid regions
-
-flow_out = opticalFlow(flow_in_x, flow_in_y);
--- start_training.m
+++
@@ -1,5 +0,0 @@
-
-% pyrunfile("./python_scripts/raft_finetuning_pseudolabel.py '--stage' '300'");
-% pyrunfile("./python_scripts/raft_finetuning_pseudolabel.py '--stage' '600'");
-% pyrunfile("./python_scripts/raft_finetuning_pseudolabel.py '--stage' '900'");
-pyrunfile("./python_scripts/raft_finetuning_pseudolabel.py '--stage' '2000'");
